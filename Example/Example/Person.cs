e74f7233de7a4e79342a9223a0f6f93eb057fdb4
Lawrence Cheung <lcheunglci@gmail.com>
2020-11-19 19:44:48 -0800

    Add Person and update main

---
+++ Example/Example/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+}
--- Example/Example/Program.cs
+++ Example/Example/Program.cs
@@ -6,7 +6,12 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Example!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Person me = new Person() { Name = name, ID = 1 };
+            Console.WriteLine("Hello, " + me.Name + ", Welcome to the class.");
+
         }
     }
 }
