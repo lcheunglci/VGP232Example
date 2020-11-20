54248f00b4d404f7cb607206fab4aa19edd9720d
Lawrence Cheung <lcheunglci@gmail.com>
2020-11-19 20:06:03 -0800

    Add warrior and update the constructor in Person

---
+++ Example/Example/Warrior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+    public class Warrior : Person
+    {
+        public int Damage { get; set; }
+
+        public Warrior(string name, int id, int damage) : base(name, id)
+        {
+            Damage = damage;
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Doing {0} damage", Damage);
+        }
+    }
+}
